0ede4e931d968687ae0abe907eba4ef7a25bb1db
ebb systems <info@engineeredbybytes.com>
2025-10-23 16:19:30 +0100

    Update wizardcd.rb

--- Formula/wizardcd.rb
+++ Formula/wizardcd.rb
@@ -1,1 +1,16 @@
+class Wizardcd < Formula
+  desc "One config. One command. Continuous magic. 🪄"
+  homepage "https://wizardcd.com"
+  url "https://github.com/engineeredbybytes/wizardcd-releases/releases/download/v1.0.8/wizardcd-1.0.8-macos.tar.gz"
+  sha256 "<REPLACE_WITH_SHA256>"
+  license "MIT"
+  version "1.0.8"
 
+  def install
+    bin.install "wizard"
+  end
+
+  test do
+    system "#{bin}/wizard", "--version"
+  end
+end
