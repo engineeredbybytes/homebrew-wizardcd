dd8ba864243a18989f52d489e2996b1aedf5fccf
ebb systems <info@engineeredbybytes.com>
2025-10-25 19:52:34 +0100

    Update wizardcd.rb

--- Formula/wizardcd.rb
+++ Formula/wizardcd.rb
@@ -1,24 +1,24 @@
 class Wizardcd < Formula
-desc "WizardCD – One Config. One Command. Continuous Magic."
-homepage "https://wizardcd.com"
-version "1.2.38"
-license "MIT"
+  desc "WizardCD – One Config. One Command. Continuous Magic."
+  homepage "https://wizardcd.com"
+  version "1.2.38"
+  license "MIT"
 
-on_macos do
-if Hardware::CPU.arm?
-url "https://github.com/engineeredbybytes/wizardcd-releases/releases/download/v#{version}/wizardcd-darwin-arm64.tar.gz"
-sha256 "88f4b618220fbd6e41a404f8f1ea88e5be803c2552c2f860444c8779a17562a4"
-else
-url "https://github.com/engineeredbybytes/wizardcd-releases/releases/download/v#{version}/wizardcd-darwin-amd64.tar.gz"
-sha256 "88f4b618220fbd6e41a404f8f1ea88e5be803c2552c2f860444c8779a17562a4"
-end
-end
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/engineeredbybytes/wizardcd-releases/releases/download/v#{version}/wizardcd-darwin-arm64.tar.gz"
+      sha256 "88f4b618220fbd6e41a404f8f1ea88e5be803c2552c2f860444c8779a17562a4"
+    else
+      url "https://github.com/engineeredbybytes/wizardcd-releases/releases/download/v#{version}/wizardcd-darwin-amd64.tar.gz"
+      sha256 "88f4b618220fbd6e41a404f8f1ea88e5be803c2552c2f860444c8779a17562a4"
+    end
+  end
 
-def install
-bin.install "wizard"
-end
+  def install
+    bin.install "wizard"
+  end
 
-test do
-assert_match "WizardCD version", shell_output("\#{bin}/wizard --version")
-end
+  test do
+    assert_match "WizardCD version", shell_output("#{bin}/wizard --version")
+  end
 end
